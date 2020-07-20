5ae21a34e9d5a68aea723dae76b91d5783eef4c3
Nitish Mehrotra <nitishmehrotra@gmail.com>
2020-07-20 14:14:08 +0530

    Adding fonts and icons to the website.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,31 @@
 module.exports = {
   plugins: [
     {
+      resolve: `gatsby-plugin-sass`,
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        icon: `assets/icons/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
       resolve: `gatsby-theme-garden`,
       options: {
         contentPath: `${__dirname}/content`,
         rootNote: `/home`,
+        ignore: [
+          "**/.cache/**",
+          "**/.git/**",
+          "**/.vscode/**",
+          "**/assets/**",
+          "**/node_modules/**",
+          "**/public/**",
+        ],
       },
     },
   ],
   siteMetadata: {
-    title: `Developers Brain`,
+    title: `Developers Brain 🧠`,
   },
 }
