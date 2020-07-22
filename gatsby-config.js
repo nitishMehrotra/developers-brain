db5f66f7a202fdd087797a2baa4c20cb87fba1ef
Nitish Mehrotra <nitishmehrotra@gmail.com>
2020-07-22 18:11:01 +0530

    Updated fonts in the project.

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,10 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [`Nunito Sans`],
+        fonts: [
+          `Nunito Sans\:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i`,
+          `Inter\:100,200,300,400,500,600,700,800,900`,
+        ],
         display: "swap",
       },
     },
